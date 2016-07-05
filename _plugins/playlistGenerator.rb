14b31628f7db4e912f4761a9ee5bfdb9a1aa05ad
chefkoch <tales.of.trinity.lps@gmail.com>
2016-07-05 12:43:47 +0200

    playlist funktioniert aber nicht auto.

--- _plugins/playlistGenerator.rb
+++ _plugins/playlistGenerator.rb
@@ -4,14 +4,22 @@ module PlaylistGenerator
     require 'open-uri'
 
     def generate(site)
-      url = "https://www.googleapis.com/youtube/v3/playlistItems?part=contentDetails&key=XXX&channelId=UCn0PbkRMNmQ-MAFvMWrhc-A&playlistId=PLsyA7GBLQIf5cx-fCtRNZoYQfxnGaW_R3"
-
-      videos = JSON.load(open(url))
+      playlists = "https://www.googleapis.com/youtube/v3/playlists?part=id&key=" + site.config['youtube_token'] + "&channelId=" + site.config['youtube_channel']
+      playlists = JSON.load(open(playlists))
 
       site.data['videos'] = Array.new
 
-      for video in videos['items']
-        site.data['videos'] << video['contentDetails']['videoId']
+      for playlist in playlists['items']
+        playlistItems = "https://www.googleapis.com/youtube/v3/playlistItems?part=contentDetails&key=" + site.config['youtube_token'] + "&channelId="+ site.config['youtube_token'] + "&playlistId=" + playlist['id']
+        videos = JSON.load(open(playlistItems))
+
+        first = true
+        for video in videos['items']
+          if first == true
+            first = false
+            site.data['videos'] << video['contentDetails']['videoId'] + "?list=" + playlist['id']
+          end
+        end
       end
     end
   end
